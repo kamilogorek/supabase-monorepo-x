840774e373c100f7f69b4b3c275d620d4348cd9f
Kamil Ogórek <kamil.ogorek@gmail.com>
2024-02-20 21:23:43 +0100

    Update DB schema

---
+++ packages/www/supabase/seed.sql
@@ -0,0 +1,5 @@
+insert into test values (1, now(), 'www seed');
+insert into test values (2, now(), 'www seed');
+insert into test values (3, now(), 'www seed');
+insert into test values (4, now(), 'www seed');
+insert into test values (5, now(), 'www seed');
